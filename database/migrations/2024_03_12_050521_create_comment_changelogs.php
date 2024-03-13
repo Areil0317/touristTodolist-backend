d0ff4995e47b273f26d4682cc65f08119e0c9a27
iigmir <roc120j@gmail.com>
2024-03-13 10:59:54 +0800

    Fix trigger

--- database/migrations/2024_03_12_050521_create_comment_changelogs.php
+++ database/migrations/2024_03_12_050521_create_comment_changelogs.php
@@ -21,7 +21,7 @@ return new class extends Migration
         Schema::table("comments", function (Blueprint $table) {
             DB::unprepared('CREATE TRIGGER log_comment_changing AFTER UPDATE ON `comments` FOR EACH ROW
             BEGIN
-                INSERT INTO comment_changelogs(`cid`, `before`, `after`, `created_at`) VALUES(OLD.id, OLD.comment, NEW.comment, NOW());
+                INSERT INTO comment_changelogs(`cid`, `before`, `after`, `created_at`) VALUES(OLD.cid, OLD.comment, NEW.comment, NOW());
             END');
         });
     }
