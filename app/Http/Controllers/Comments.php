6195f9300440f7b27bc865da7c8f5ba88e307c04
iigmir <roc120j@gmail.com>
2024-03-17 02:03:14 +0800

    Add auth

--- app/Http/Controllers/Comments.php
+++ app/Http/Controllers/Comments.php
@@ -7,6 +7,11 @@ use App\Models\CommentModel;
 
 class Comments extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware("auth:sanctum")->except(["index", "show"]);
+    }
+
     /**
      * Display a listing of the resource.
      */
