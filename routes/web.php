5acd6fc47304c920c777636fcbc9167df54cdb45
iigmir <roc120j@gmail.com>
2024-03-20 16:12:26 +0800

    頭像圖片使用

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::get("/", function () {
     return response([ "message" => "Hello World" ]);
 });
+
+Route::get("/avatars/{filename}", function ($filename) {
+    $path = storage_path("app/public/avatars/$filename");
+    if (!Storage::exists("public/avatars/$filename")) {
+        abort(400);
+    }
+    return response()->file($path);
+})->where("filename", ".*");
