e2961cf8d7328623e73a1cc116961ffb0ee7abdc
iigmir <roc120j@gmail.com>
2024-03-26 16:05:37 +0800

    I was wrong. Delete the attributes

--- app/Models/JourneyModel.php
+++ app/Models/JourneyModel.php
@@ -10,25 +10,6 @@ class JourneyModel extends Model
     protected $table = 'journey';
     protected $primaryKey = 'jid';
     public $timestamps = false;
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        "jchecked" => "boolean",
-    ];
-    /**
-     * Mutate the attribute value to boolean.
-     * "1" is true and "0" is false.
-     *
-     * @param  mixed  $value
-     * @return bool
-     */
-    public function getJcheckedAttribute($value)
-    {
-        return $value === "1";
-    }
 
     public function journeyProjects()
     {
