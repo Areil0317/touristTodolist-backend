d87b56db4e4a493d95c66fa094eb1baccd47ec25
iigmir <roc120j@gmail.com>
2024-03-26 16:00:49 +0800

    Add check as boolean

--- app/Models/JourneyProjectModel.php
+++ app/Models/JourneyProjectModel.php
@@ -10,6 +10,25 @@ class JourneyProjectModel extends Model
     protected $table = 'journeyproject';
     protected $primaryKey = 'jpid';
     public $timestamps = false;
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        "jpchecked" => "boolean",
+    ];
+    /**
+     * Mutate the attribute value to boolean.
+     * "1" is true and "0" is false.
+     *
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function getJpcheckecAttribute($value)
+    {
+        return $value === "1";
+    }
 
     public function project()
     {
